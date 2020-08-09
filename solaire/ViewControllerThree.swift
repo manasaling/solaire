68471e01b753b5603eae5d8baee9ee36c9c54fd8
manasa <lgmanasa@gmail.com>
2020-08-09 14:19:38 -0700

    wireframe

---
+++ solaire/ViewControllerThree.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerThree.swift
+//  solaire
+//
+//  Created by Apple on 8/9/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerThree: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    private func setupNavigationBarItems(){
+           print(123)
+           
+       }
+
+       
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+  
+
+}
